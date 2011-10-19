0b50ea06055b336ea968a643b194d0744ac07677
burisu <burisu@oneiros.fr>
2011-10-19 22:48:42 +0200

    Adds Rakefile, tests and Bundler

--- lib/open-document.rb
+++ lib/open-document.rb
@@ -1,2 +1,7 @@
 # encoding: utf-8
 
+module OpenDocument
+
+  
+
+end
---
+++ test/test_open_document.rb
@@ -0,0 +1,9 @@
+class TestOpenDocument < Test::Unit::TestCase
+  
+  def test_existence
+    assert_not_nil OpenDocument
+  end
+
+end
+
+
